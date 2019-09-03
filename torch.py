cbe8111920c2b88c7ec0783f383c13b69a24dfb1
Paulius Kutka <kutka.paulius@gmail.com>
2019-09-03 12:29:21 +0300

    Started text classification tutorial - nothing works

---
+++ torch.py
@@ -0,0 +1,14 @@
+import os
+
+import torchtext
+from torchtext.datasets import text_classification
+
+import torch
+
+NGRAMS = 2
+if not os.path.isdir('./.data'):
+    os.mkdir('./.data')
+train_dataset, test_dataset = text_classification.DATASETS['AG_NEWS'](
+    root='./.data', ngrams=NGRAMS, vocab=None)
+BATCH_SIZE = 16
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
